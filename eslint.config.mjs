1261d3151745a75969ac18b06ca8bfa2bc8c697f
Khenziii <khenzii.was.taken@gmail.com>
2024-08-09 19:51:05 +0200

    feat: initial commit!

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  {files: ["./**/*.ts"]},
+  {languageOptions: { globals: globals.browser }},
+  ...tseslint.configs.recommended,
+];
