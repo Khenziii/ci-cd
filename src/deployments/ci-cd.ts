d0e62f66040c655d3ab5adbb6407439830633734
Khenziii <khenzii.was.taken@gmail.com>
2024-08-14 18:57:23 +0200

    feat(deployments): add `ci-cd`

---
+++ src/deployments/ci-cd.ts
@@ -0,0 +1,12 @@
+import { Deployment } from "./deployment.js";
+
+const commands_list: string[] = [
+    "cd ~/ci-cd",
+    "npm install",
+    "npm run build",
+    "pm2 restart ci-cd",
+];
+const route_name: string = "ci-cd";
+
+export const ciCdDeployment = new Deployment(commands_list, route_name);
+
--- src/deployments/index.ts
+++ src/deployments/index.ts
@@ -1,6 +1,8 @@
 import { khenziiDevDeployment } from "./khenzii-dev.js";
+import { ciCdDeployment } from "./ci-cd.js";
 
 export const deployments = [
     khenziiDevDeployment,
+    ciCdDeployment,
 ];
 
