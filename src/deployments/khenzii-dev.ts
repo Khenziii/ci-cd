616b97e8d7d43deff061c79945fab59591152dab
Khenziii <khenzii.was.taken@gmail.com>
2024-08-14 19:17:40 +0200

    Revert "test: check if ci-cd is correctly managing itself"
    This reverts commit 53c7b7a0d8ccac0605d5b0e285cfe32fd1c11f36.
    
    It indeed is managing itself!

--- src/deployments/khenzii-dev.ts
+++ src/deployments/khenzii-dev.ts
@@ -1,6 +1,10 @@
 import { Deployment } from "./deployment.js";
 
-const commands_list: string[] = [];
+const commands_list: string[] = [
+    "sleep 10",
+    "echo 'hello world!'",
+    "notify-send 'hello!'",
+];
 const route_name: string = "khenzii-dev";
 
 export const khenziiDevDeployment = new Deployment(commands_list, route_name);
