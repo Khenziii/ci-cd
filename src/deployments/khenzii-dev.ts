74726bf66a05167b5ad2ad1737001510cb2cc93f
Khenziii <khenzii.was.taken@gmail.com>
2024-08-14 19:21:18 +0200

    fix(deployments): remove commands from `khenzii-dev`
    I'm going to write this deployment later

--- src/deployments/khenzii-dev.ts
+++ src/deployments/khenzii-dev.ts
@@ -1,10 +1,6 @@
 import { Deployment } from "./deployment.js";
 
-const commands_list: string[] = [
-    "sleep 10",
-    "echo 'hello world!'",
-    "notify-send 'hello!'",
-];
+const commands_list: string[] = [];
 const route_name: string = "khenzii-dev";
 
 export const khenziiDevDeployment = new Deployment(commands_list, route_name);
