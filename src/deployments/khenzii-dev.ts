092cf31ee80128b1d1475aa2cee987aad9f86bfd
Khenziii <khenzii.was.taken@gmail.com>
2024-08-15 19:49:19 +0200

    feat(deployments): write `khenzii-dev` deployment

--- src/deployments/khenzii-dev.ts
+++ src/deployments/khenzii-dev.ts
@@ -1,6 +1,9 @@
 import { Deployment } from "./deployment.js";
 
-const commands_list: string[] = [];
+const commands_list: string[] = [
+    "docker pull khenzii/khenzii-dev:latest",
+    "pm2 restart khenzii-dev",
+];
 const route_name: string = "khenzii-dev";
 
 export const khenziiDevDeployment = new Deployment(commands_list, route_name);
