8213c61323f943225d4e9a017490a33bca5d8228
Khenziii <khenzii.was.taken@gmail.com>
2024-08-19 21:08:02 +0200

    feat(tests): add a deployment fail test

--- src/deployments/deployment.test.ts
+++ src/deployments/deployment.test.ts
@@ -1,4 +1,15 @@
-import { execute_command } from "./deployment";
+import { DeploymentError } from "./errors";
+import { execute_command, Deployment } from "./deployment";
+
+const partiallyInvalidDeployment: Deployment = new Deployment(
+    [
+        `echo "I'm a working command, but the next one will fail"`,
+        "false",
+        `echo "I'm a valid command, that shouldn't be reached"`,
+    ],
+    "partially-invalid-deployment",
+    true,
+);
 
 describe("Deployments", () => {
     it("Should resolve a valid command into it's standard output", async () => {
@@ -9,5 +20,9 @@ describe("Deployments", () => {
     it("Should fail to execute an invalid command", async () => {
         await expect(execute_command("false")).rejects.toBeDefined();
     });
+
+    it("A whole deployment should fail if an invalid command encountered, and `fail_fast` is set to true", async () => {
+        await expect(partiallyInvalidDeployment.run()).rejects.toThrow(DeploymentError);
+    })
 });
 
