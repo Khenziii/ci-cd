8a1f5b007991708fe71d8722a81938d438d481b2
Khenziii <khenzii.was.taken@gmail.com>
2024-08-19 18:30:13 +0200

    feat(tests): write root route test

--- src/app.test.ts
+++ src/app.test.ts
@@ -5,7 +5,16 @@ import request from "supertest";
 describe("Main App", () => {
     it("Should show an 404 error after requesting a non-existent route", async () => {
         const res = await request(ciCd.app).get("/not_found");
+
         expect(JSON.parse(res.text)["message"]).toEqual("This deployment doesn't exist!");
+        expect(res.statusCode).toEqual(404);
+    })
+
+    it("Should show an info message after requesting the root route", async () => {
+        const res = await request(ciCd.app).get("/");
+
+        expect(JSON.parse(res.text)["message"]).toEqual("Provide app name via the route!");
+        expect(res.statusCode).toEqual(400);
     })
     
     afterAll(async () => {
