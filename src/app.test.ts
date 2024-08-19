477e64a9c07df78d1de210ab8cc1919992d16395
Khenziii <khenzii.was.taken@gmail.com>
2024-08-19 18:50:07 +0200

    feat(tests): add invalid secret test

--- src/app.test.ts
+++ src/app.test.ts
@@ -18,24 +18,31 @@ describe("Main App", () => {
 
         expect(JSON.parse(res.text)["message"]).toEqual("This deployment doesn't exist!");
         expect(res.statusCode).toEqual(404);
-    })
+    });
 
     it("Should show an info message after requesting the root route", async () => {
         const res = await request(ciCd.app).get("/");
 
         expect(JSON.parse(res.text)["message"]).toEqual("Provide app name via the route!");
         expect(res.statusCode).toEqual(400);
-    })
+    });
 
     it("Should show an 'invalid credentials' error when using NO secret", async () => {
         const res = await request(ciCd.app).get(`/${deployments[0].route_name}`);
 
         expect(JSON.parse(res.text)["message"]).toMatch("Include the secret in a query parameter\.");
         expect(res.statusCode).toEqual(401);
-    })
-    
+    });
+
+    it("Should show an 'invalid credentials' error when using invalid secret", async () => {
+        const res = await request(ciCd.app).get(`/${deployments[0].route_name}?secret=invalid-secret`);
+
+        expect(JSON.parse(res.text)["message"]).toMatch("Invalid secret!");
+        expect(res.statusCode).toEqual(401);
+    });
+
     afterAll(async () => {
         await ciCd.stop();
-    })
-})
+    });
+});
 
