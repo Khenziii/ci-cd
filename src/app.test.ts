8a28daf9b96b61a196cb83ffd6bb142ba87d32df
Khenziii <khenzii.was.taken@gmail.com>
2024-08-19 18:43:25 +0200

    feat(tests): add another root route test

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,6 +1,16 @@
-import { ciCd } from "./index";
-
 import request from "supertest";
+
+import { App } from "./app";
+import { Deployment } from "./deployments/deployment";
+
+const secret: string = "test-secret";
+const deployments: Deployment[] = [
+    new Deployment(
+        ["echo 'I'm a test!'"],
+        "/test-deployment",
+    ),
+];
+const ciCd: App = new App(3000, secret, deployments);
 
 describe("Main App", () => {
     it("Should show an 404 error after requesting a non-existent route", async () => {
@@ -15,6 +25,13 @@ describe("Main App", () => {
 
         expect(JSON.parse(res.text)["message"]).toEqual("Provide app name via the route!");
         expect(res.statusCode).toEqual(400);
+    })
+
+    it("Should show an 'invalid credentials' error when using NO secret", async () => {
+        const res = await request(ciCd.app).get(`/${deployments[0].route_name}`);
+
+        expect(JSON.parse(res.text)["message"]).toMatch("Include the secret in a query parameter\.");
+        expect(res.statusCode).toEqual(401);
     })
     
     afterAll(async () => {
