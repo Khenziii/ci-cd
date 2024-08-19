d6f59d39dff9a5bdb8fe63de9e7bcf202f6a6b89
Khenziii <khenzii.was.taken@gmail.com>
2024-08-19 20:38:47 +0200

    feat(tests): add an invalid command test

--- src/deployments/deployment.test.ts
+++ src/deployments/deployment.test.ts
@@ -4,6 +4,10 @@ describe("Deployments", () => {
     it("Should resolve a valid command into it's standard output", async () => {
         const stdout = await execute_command(`echo "I'm a test!"`);
         expect(stdout).toMatch("I'm a test!");
-    })
-})
+    });
+
+    it("Should fail to execute an invalid command", async () => {
+        await expect(execute_command("false")).rejects.toBeDefined();
+    });
+});
 
