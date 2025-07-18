acd86041875af39776147af8ae0214e33a39caec
Khenziii <khenzii.was.taken@gmail.com>
2025-07-19 00:00:20 +0200

    feat(deployments): update `khenzii-dev` deployment

--- src/deployments/khenzii-dev.ts
+++ src/deployments/khenzii-dev.ts
@@ -2,7 +2,8 @@ import { Deployment } from "./deployment";
 
 const commands_list: string[] = [
     "docker pull khenzii/khenzii-dev:latest",
-    "pm2 restart khenzii-dev",
+    "docker rm -f $(docker ps -q --filter 'ancestor=khenzii/khenzii-dev')",
+    "docker run --env-file /home/khenzii/khenzii-dev-secrets.env -p 3003:3000 --restart unless-stopped -d khenzii/khenzii-dev",
 ];
 const route_name: string = "khenzii-dev";
 
